fb00ec8a234210def5fcceb11e3a543f9f693bf2
SyedAli310 <alizaidi310@gmail.com>
2021-12-30 22:31:16 +0530

    script fix

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const { encode, decode } = require("./middlewares/encode-decode");
 
 app.use(express.json());
 
-app.get("/numToChar", async (req, res) => {
-  const { num } = req.query;
-    const result = await decode(num);
-    res.status(200).json({num, result});
+app.get("/decode", async (req, res) => {
+  const { numCode } = req.query;
+    const result = await decode(numCode);
+    res.status(200).json({numCode, result});
 });
-app.get("/charToNum", async (req, res) => {
+app.get("/encode", async (req, res) => {
   const { str } = req.query;
   const result = await encode(str);
   res.status(200).json({ str, result });
