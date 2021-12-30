4286cdf2c229e8b0076ba9deb5b30747ff151b28
SyedAli310 <alizaidi310@gmail.com>
2021-12-31 00:23:23 +0530

    formatting

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const validSchemes = Object.keys(schemeCollection);
 app.get("/decode", async (req, res) => {
   const { code } = req.query;
   if (!code) {
-    res.status(400).json({ error: "Please provide a code" });
+    res.status(400).json({ error:true, msg: "please provide a code" });
   } else {
-      const schemeName = `scheme_${code.split('@').reverse()[0]}`;
+    const schemeName = `scheme_${code.split("@").reverse()[0]}`;
     const codingScheme = schemeCollection[schemeName].decode;
-    const actualCode = code.split('@').reverse()[1];
+    const actualCode = code.split("@").reverse()[1];
     console.log(codingScheme);
     try {
       const decoded = await decode(actualCode, codingScheme);
-      res.status(200).json({ code, decoded, 'schemeUsed':schemeName });
+      res.status(200).json({ error:false , code, decoded, schemeUsed: schemeName });
     } catch (err) {
-      res.status(400).json(err.message);
+      res.status(400).json({error:true, msg:err.message});
     }
   }
 });
@@ -30,18 +30,18 @@ app.get("/encode", async (req, res) => {
   if (!scheme || !str) {
     res
       .status(400)
-      .json({ error: "Please provide a string and a coding scheme" });
+      .json({ error:true, msg: "please provide a string and a coding scheme" });
   } else {
     if (!validSchemes.includes(scheme)) {
-      res.status(400).json({ error: "Please provide a valid scheme" });
+      res.status(400).json({ error:true, msg: "please provide a valid scheme" });
     } else {
       const codingScheme = schemeCollection[scheme].encode;
       const identifier = scheme.split("_").reverse()[0];
       try {
         const encoded = (await encode(str, codingScheme)) + "@" + identifier;
-        res.status(200).json({ str, encoded, scheme });
+        res.status(200).json({error:false, text:str, encoded, scheme });
       } catch (err) {
-        res.status(400).json(err.message);
+        res.status(400).json({error:true, msg:err.message});
       }
     }
   }
