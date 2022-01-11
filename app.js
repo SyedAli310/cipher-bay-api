6715a255a52084fe5ab066b044bd86e13b1ac193
SyedAli310 <alizaidi310@gmail.com>
2022-01-12 03:00:32 +0530

    added and used scheme router + added database connection on server start

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 
+//db connection
+const connectDB = require("./db/connect");
+
 //middlewares
 const auth = require("./middlewares/auth");
+const adminCheck = require("./middlewares/adminCheck");
 
 //routers
 const cipherRouter = require("./routes/cipher");
+const schemeRouter = require("./routes/scheme");
 
 app.set("trust proxy", 1);
 app.use(
@@ -39,6 +44,8 @@ app.get("/github/repo", (req, res) => {
 
 //routes
 app.use("/api/v1/cipher", auth, cipherRouter);
+app.use("/api/v1/scheme", adminCheck, schemeRouter);
+
 // Handling non matching request from the client
 app.use((req, res, next) => {
   res.status(404).sendFile(__dirname + "/public/404.html");
@@ -48,6 +55,15 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
