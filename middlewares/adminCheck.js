a9ad957c60915bd77698c506be870c32d40c0434
SyedAli310 <alizaidi310@gmail.com>
2022-01-12 03:46:46 +0530

    getting admin secret from header instead of query param

--- middlewares/adminCheck.js
+++ middlewares/adminCheck.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 const adminCheck = (req, res, next) => {
     // get admin secret from header
-    // const adminSecret = req.header('adminSecret');
-    const {adminSecret} = req.query;
+    const adminSecret = req.header('adminSecret');
     if(!adminSecret){
-        return res.status(401).json({error: true, msg: "please provide an admin secret to access this resource"});
+        return res.status(401).json({
+            error: true, 
+            msg: "please provide an admin secret to access this resource",
+        });
     }
     if(adminSecret !== process.env.ADMIN_SECRET){
-        return res.status(401).json({error: true, msg: "you are not authorized or don't have admin rights to access this resource"});
+        return res.status(401).json({
+            error: true, 
+            msg: "you are not authorized or don't have admin rights to access this resource",
+            providedSecret: adminSecret
+        });
     }
     next();
 };
