9295ea4a00c11d2a87b6d46359513fbef852f797
SyedAli310 <alizaidi310@gmail.com>
2022-01-12 03:47:03 +0530

    modified endpoint responses

--- controllers/scheme.js
+++ controllers/scheme.js
@@ -3,14 +3,21 @@ const Scheme = require("../models/Scheme");
 const viewSchemes = async (req, res) => {
     try {
         const schemes = await Scheme.find();
+        if(!schemes) {
+            return res.status(404).json({
+                error: true,
+                msg: "Could not fetch any schemes from the database"
+            });
+        }
         res.status(200).json({
-        success: true,
-        data: schemes
+        error: false,
+        scheme: schemes,
+        count: schemes.length,
         });
     } catch (error) {
         res.status(500).json({
-        success: false,
-        error: error.message
+        error: true,
+        msg: error.message
         });
     }
 }
