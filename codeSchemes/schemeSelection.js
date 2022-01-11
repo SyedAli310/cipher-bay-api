e1d66b3fd9b93d37b54a905cc32565f0b65b3b3d
SyedAli310 <alizaidi310@gmail.com>
2022-01-12 03:01:07 +0530

    removed db connection as it establishes at server start

--- codeSchemes/schemeSelection.js
+++ codeSchemes/schemeSelection.js
@@ -1,11 +1,8 @@
-require("dotenv").config();
-const connectDB = require("../db/connect");
 const Scheme = require("../models/Scheme");
 
 // fetch schemes from mongoDB
 const fetchSchemes = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
     const schemes = await Scheme.find();
     return schemes;
   } catch (error) {
