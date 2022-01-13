1f0daf6e7543df2ee8372df62e5fdf826b3baae3
SyedAli310 <alizaidi310@gmail.com>
2022-01-13 22:49:43 +0530

    scheme : added add scheme route

--- routes/scheme.js
+++ routes/scheme.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { viewSchemes } = require("../controllers/scheme");
+const { viewSchemes, addScheme } = require("../controllers/scheme");
 
 router.route('/view').get(viewSchemes);
 router.route('/view/:id').get(viewSchemes);
+router.route('/add').post(addScheme);
 
 module.exports = router;
 
