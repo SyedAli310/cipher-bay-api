a84ad21d62130f3a82c9cfc631b75ce659e5ad43
SyedAli310 <alizaidi310@gmail.com>
2022-03-02 21:04:17 +0530

    cipher : added admin login route

--- routes/cipher.js
+++ routes/cipher.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { encoder, decoder, schemes } = require("../controllers/cipher");
+const { encoder, decoder, schemes, adminLogin } = require("../controllers/cipher");
 
 router.route('/encode').post(encoder);
 router.route('/decode').post(decoder);
 router.route('/schemes').get(schemes);
+router.route('/adminLogin').post(adminLogin);
 
 module.exports = router;
