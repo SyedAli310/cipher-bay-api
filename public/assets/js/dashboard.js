190dd4017b0cb6ebc0608f3158a2bdc25f13a074
SyedAli310 <alizaidi310@gmail.com>
2022-03-06 16:08:27 +0530

    dashboard : added icons + UI revamp/tweaks for dashboard

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -34,6 +34,28 @@ const getSchemes = async () => {
   }
 };
 
+// make a function to expand all schemes
+const toggleAllSchemes = (action) => {
+  const schemeListItems = schemeList.querySelectorAll(".scheme-list-item");
+  const allToggleBtns = document.querySelectorAll("[data-scheme-toggle]");
+  allToggleBtns.forEach((btn) => {
+    if(action === "open") {
+      btn.classList.add("open");
+    }
+    else if(action === "close") {
+      btn.classList.remove("open");
+    }
+  })
+  schemeListItems.forEach((item) => {
+    if(action === "open") {
+      item.classList.add("expanded");
+    }
+    else if(action === "close") {
+      item.classList.remove("expanded");
+    }
+  });
+}
+
 const schemeToggleBinder = () => {
   const allToggleBtns = document.querySelectorAll("[data-scheme-toggle]");
   allToggleBtns.forEach((btn) => {
@@ -59,8 +81,12 @@ const makeObjUI = (obj, title) => {
       return;
     }
     const objUIItem = document.createElement("div");
-    objUIItem.classList.add("obj-ui-item");
-    objUIItem.innerHTML = `<span>${key}&nbsp;</span>&harr;<span>&nbsp;${value}</span>`;
+    objUIItem.classList.add("obj-item");
+    objUIItem.innerHTML = `
+      <span>${key}</span>
+      <span>&harr;</span>
+      <span>${value}</span>
+    `;
     objUI.appendChild(objUIItem);
   })
   UI.appendChild(objTitle);
@@ -71,7 +97,7 @@ const makeObjUI = (obj, title) => {
 const showDashboard = async () => {
   schemeList.innerHTML = "<span style='color:var(--MAIN__ACCENT__COLOR);'>loading...</span>";
   const schemes = await getSchemes();
-  // console.log(schemes);
+  console.log(schemes);
   if(schemes.error) {
     dashHeaderBtnWrap.innerHTML = ` <a class="btn login-btn a-reset" href="/login">Login</a>`;
     resMsg.classList.add("show");
@@ -83,29 +109,44 @@ const showDashboard = async () => {
   dashHeaderBtnWrap.innerHTML = ` <a class="btn logout-btn a-reset" href="/logout">Logout</a>`;
   const allSchemes = schemes.scheme;
   const schemesCount = schemes.schemes_count;
-  dynamicSchemeTitle.innerHTML = `<div>Total Schemes Added - ${schemesCount}</div><button class='btn btn-sm' id='refresh-schemes' title='refresh schemes'>&#x21bb;</button>`;
+  dynamicSchemeTitle.innerHTML = `
+  <div>Schemes added-${schemesCount} </div>
+  <div class='scheme-cta-btns'>
+    <button class='btn btn-sm' id='refresh-schemes' title='refresh schemes'>&#x21bb;</button>
+    <button class='btn btn-sm' id='expand-all' title='expand all'>expand &#9660;</button>
+    <button class='btn btn-sm' id='close-all' title='close all'>shrink  &#9650;</button>
+  </div>
+  `;
   document.querySelector("#refresh-schemes").addEventListener("click", () => {showDashboard()});
+  document.querySelector("#expand-all").addEventListener("click", () => {toggleAllSchemes('open')});
+  document.querySelector("#close-all").addEventListener("click", () => {toggleAllSchemes('close')});
   allSchemes.forEach((scheme) => {
     const schemeLi = document.createElement("li");
     schemeLi.classList.add('scheme-list-item');
     schemeLi.innerHTML = `
     <div class="scheme-title">
-      <span class="alias">${scheme.alias} <br> <small>Added: ${new Date(scheme.createdAt).toDateString().slice(3)}</small></span>
-      <span class="name">${scheme.name} &nbsp; <button class='btn btn-sm scheme-toggle-btn' data-scheme-toggle><span>&#9660;</span></button></span>
+      <span class="alias">${scheme.alias}</span>
+      <span class="name">${scheme.name} &nbsp; <button class='btn btn-md scheme-toggle-btn' data-scheme-toggle><span>&#9660;</span></button></span>
     </div>
     `;
     const schemeBody = document.createElement("div");
     schemeBody.classList.add("scheme-body");
-    schemeBody.appendChild(makeObjUI(scheme.encode, 'Encode Key'));
-    schemeBody.appendChild(makeObjUI(scheme.decode, 'Decode Key'));
+    const schemeBodyHeader = document.createElement("div");
+    schemeBodyHeader.classList.add("scheme-body-header");
+    schemeBodyHeader.innerHTML = `
+    <small>🕑 Added: ${new Date(scheme.createdAt).toDateString().slice(3)}</small>
+    <div class='scheme-body-btns'>
+      <button class='btn btn-sm' id='delete-scheme' title='delete scheme'><img src="/assets/img/icons/trash-outline.svg" alt="del" /></button>
+      <button class='btn btn-sm' id='download-scheme' title='download scheme'><img src="/assets/img/icons/cloud-download-outline.svg" alt="save" /></button>
+    </div>
+    `;
+    schemeBody.appendChild(schemeBodyHeader);
+    schemeBody.appendChild(makeObjUI(scheme.encode, `cipher key🔹${scheme.alias}`));
     schemeLi.appendChild(schemeBody);
     schemeList.appendChild(schemeLi);
   });
 
   schemeToggleBinder();
 }
-
-//<pre>${JSON.stringify(scheme.encode, null, '\t')}</pre>
-//<pre>${JSON.stringify(scheme.decode, null, '\t')}</pre>
 
 window.onload = showDashboard();
