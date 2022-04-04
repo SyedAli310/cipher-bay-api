99e309de8533603bdcba4c5c19a67c1346ff529f
SyedAli310 <alizaidi310@gmail.com>
2022-04-05 00:40:45 +0530

    delete scheme : added delete scheme route + implemented the delete controller

--- routes/scheme.js
+++ routes/scheme.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { viewSchemes, addScheme } = require("../controllers/scheme");
+const {
+  viewSchemes,
+  addScheme,
+  deleteScheme,
+} = require("../controllers/scheme");
 
-router.route('/view').get(viewSchemes);
-router.route('/view/:id').get(viewSchemes);
-router.route('/add').post(addScheme);
+router.route("/view").get(viewSchemes);
+router.route("/view/:id").get(viewSchemes);
+router.route("/add").post(addScheme);
+router.route("/delete/:id").delete(deleteScheme);
 
 module.exports = router;
-
