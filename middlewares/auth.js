4de712fe5c6ba713ef7893b9afba8632fd96d678
SyedAli310 <alizaidi310@gmail.com>
2022-04-08 14:50:08 +0530

    auth : modified auth logic to verify api key from DB instead of local env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
+const ApiKey = require("../models/ApiKey");
 
-const auth = (req, res, next) => {
-    // get apiKey from header
-    const apiKey = req.header('apiKey');
-    if(!apiKey){
-        return res.status(401).json({error: true, msg: "please provide an api key"});
-    }
-    if(apiKey !== process.env.API_KEY){
-        return res.status(401).json({error: true, msg: "invalid api key"});
-    }
-    next();
+const auth = async (req, res, next) => {
+  // get apiKey from header
+  const apiKey = req.header("apiKey");
+  // check if apiKey exists
+  if (!apiKey) {
+    return res
+      .status(401)
+      .json({ error: true, msg: "please provide an api key" });
+  }
+  // check if apiKey is valid
+  const keyIsValid = await ApiKey.findOne({ value: apiKey });
+  if (!keyIsValid) {
+    return res.status(401).json({ error: true, msg: "invalid api key" });
+  }
+  // if apiKey is valid, continue
+  next();
 };
 
 module.exports = auth;
