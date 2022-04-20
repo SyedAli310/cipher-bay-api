05b7a9b4f54f12d2b9176d377b0ce3b2f70d6b8e
SyedAli310 <alizaidi310@gmail.com>
2022-04-20 17:52:01 +0530

    routes : made a common export for all the routers

---
+++ routes/index.js
@@ -0,0 +1,4 @@
+module.exports.navigation = require("./navigation");
+module.exports.cipher = require("./cipher");
+module.exports.scheme = require("./scheme");
+module.exports.admin = require("./admin");
